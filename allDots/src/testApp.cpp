0b889903259a70a35bd7dd081497487bd59bd134
Aaron Krajeski <aaronkrajeski@gmail.com>
2014-04-08 19:11:35 -0400

    Maybe the alldots project is not such a good idea

--- allDots/src/testApp.cpp
+++ allDots/src/testApp.cpp
@@ -3,59 +3,82 @@
 //--------------------------------------------------------------
 void testApp::setup(){
 
+	kinect.setRegistration(true);
+	kinect.init(false, false);
+	kinect.open();
+
+	grayImage.allocate(kinect.width, kinect.height);
+
+	nearThreshold = 230;
+	farThreshold = 70;
+
+	ofSetFrameRate(60);
+
 }
 
 //--------------------------------------------------------------
 void testApp::update(){
 
+	kinect.update();
+	if(kinect.isFrameNew() ) {
+		grayImage.setFromPixels(kinect.getDepthPixels(), kinect.width, kinect.height);
+
+		unsigned char * pix = grayImage.getPixels();
+		
+		int numPixels = grayImage.getWidth() * grayImage.getHeight();
+		for(int i = 0; i < numPixels; i++) {
+			if(pix[i] < nearThreshold && pix[i] > farThreshold) {
+				pix[i] = 255;
+			} else {
+				pix[i] = 0;
+			}
+		}
+
+		grayImage.flagImageChanged();
+		contourFinder.findContours(grayImage, 10, (kinect.width*kinect.height)/2, 20, false);
+	}
 }
 
 //--------------------------------------------------------------
 void testApp::draw(){
+	grayImage.draw(0, 0, kinect.width, kinect.height);
 
+	ofSetColor(255, 255, 255);
+	stringstream reportStream;
+
+	reportStream << "Far Threshold: " << farThreshold << endl
+	<< "Near Threshold: " << nearThreshold << endl;
+}
+
+//--------------------------------------------------------------
+void testApp::exit(){
+	kinect.close();
 }
 
 //--------------------------------------------------------------
 void testApp::keyPressed(int key){
-
-}
-
-//--------------------------------------------------------------
-void testApp::keyReleased(int key){
-
-}
-
-//--------------------------------------------------------------
-void testApp::mouseMoved(int x, int y ){
-
-}
-
-//--------------------------------------------------------------
-void testApp::mouseDragged(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void testApp::mousePressed(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void testApp::mouseReleased(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void testApp::windowResized(int w, int h){
-
-}
-
-//--------------------------------------------------------------
-void testApp::gotMessage(ofMessage msg){
-
-}
-
-//--------------------------------------------------------------
-void testApp::dragEvent(ofDragInfo dragInfo){ 
-
+	switch(key) {
+		case '>':
+		case '.':
+			farThreshold ++;
+			if (farThreshold > 255) farThreshold = 255;
+			break;
+			
+		case '<':
+		case ',':
+			farThreshold --;
+			if (farThreshold < 0) farThreshold = 0;
+			break;
+			
+		case '+':
+		case '=':
+			nearThreshold ++;
+			if (nearThreshold > 255) nearThreshold = 255;
+			break;
+			
+		case '-':
+			nearThreshold --;
+			if (nearThreshold < 0) nearThreshold = 0;
+			break;
+	}		
 }
--- allDots/src/testApp.h
+++ allDots/src/testApp.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "ofMain.h"
+#include "ofxKinect.h"
+#include "ofxOpenCv.h"
 
 class testApp : public ofBaseApp{
 
@@ -8,15 +10,16 @@ class testApp : public ofBaseApp{
 		void setup();
 		void update();
 		void draw();
+		void exit();
 
 		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
+
+		ofxKinect kinect;
+
+		ofxCvGrayscaleImage grayImage;
+
+		ofxCvContourFinder contourFinder;
 		
+		int nearThreshold;
+		int farThreshold;
 };
