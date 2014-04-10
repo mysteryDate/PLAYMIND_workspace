8781f379682d3bc862e6faa4a746db344e5dc8c2
Aaron Krajeski <aaronkrajeski@gmail.com>
2014-04-10 15:14:52 -0400

    Actually, with all this stuff too

---
+++ CvWithVideo/src/ofApp.cpp
@@ -0,0 +1,126 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+
+	movie.loadMovie("arms.mov");
+	bPlaying = false;
+
+	colorImg.allocate(DISPLAY_WIDTH, DISPLAY_HEIGHT);
+	grayImg.allocate(DISPLAY_WIDTH, DISPLAY_HEIGHT);
+
+	nearThreshold = 250;
+	farThreshold = 200;
+
+	ofSetFrameRate(60);
+	ofBackground(0,0,0);
+
+	tolerance = 1;
+
+
+
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+
+	movie.update();
+
+	if(movie.isFrameNew() ) {
+
+		colorImg.setFromPixels(movie.getPixels(), DISPLAY_WIDTH, DISPLAY_HEIGHT);
+		grayImg = colorImg;
+
+		unsigned char * pix = grayImg.getPixels();
+		
+		int numPixels = grayImg.getWidth() * grayImg.getHeight();
+		for(int i = 0; i < numPixels; i++) {
+		    if(pix[i] > nearThreshold or pix[i] < farThreshold) {
+		        pix[i] = 0;
+		    } 
+		}
+
+		contourFinder.findContours(grayImg, 20, (DISPLAY_WIDTH*DISPLAY_HEIGHT)/2, 10, false);
+		arms.clear();
+
+		for (int i = 0; i < contourFinder.nBlobs; ++i)
+		{
+			ofPolyline shape = ofPolyline(contourFinder.blobs[i].pts);
+			shape.simplify(tolerance);
+			armBlob arm;
+			arm.shape = shape;
+			arms.push_back(arm);
+		}
+
+	}
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+
+	//movie.draw(0, 0, DISPLAY_WIDTH, DISPLAY_HEIGHT);
+	//grayImg.draw(0, 0, DISPLAY_WIDTH, DISPLAY_HEIGHT);
+	contourFinder.draw(0, 0, DISPLAY_WIDTH, DISPLAY_HEIGHT);
+
+	ofPushStyle();
+	ofSetColor(0, 255, 0);
+	for (int i = 0; i < arms.size(); ++i)
+	{
+		arms[i].shape.draw();
+	}
+
+
+	stringstream reportStream;
+	reportStream << "Near threshold: " << nearThreshold << endl
+	<< "Far threshold: " << farThreshold << endl
+	<< "Tolerance: " << tolerance << endl
+	<< "Framerate: " << ofGetFrameRate() << endl;
+	ofDrawBitmapString(reportStream.str(), DISPLAY_WIDTH - 200, DISPLAY_HEIGHT - 50);
+	ofPopStyle();
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+	switch(key) {
+
+		case ' ':
+			if(!bPlaying) {
+				movie.play();
+				bPlaying = true;
+			}
+			else {
+				movie.stop();
+				bPlaying = false;
+			}
+			break;
+
+		case '.':
+			farThreshold++;
+			break;
+
+		case ',':
+			farThreshold--;
+			break;
+
+		case '=':
+			nearThreshold++;
+			break;
+
+		case '-':
+			nearThreshold--;
+			break;
+
+		case 't':
+			tolerance += 0.1;
+			break;
+
+		case 'r':
+			tolerance -= 0.1;
+			break;
+
+	}
+
+}
---
+++ CvWithVideo/src/ofApp.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#define DISPLAY_WIDTH 640
+#define DISPLAY_HEIGHT 480
+
+#include "ofMain.h"
+#include "ofxOpenCv.h"
+#include "ofxCv.h"
+#include "armBlob.h"
+
+class ofApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed(int key);
+
+		ofVideoPlayer	movie;
+		bool			bPlaying;
+
+		int				nearThreshold;
+		int				farThreshold;
+
+		ofxCvColorImage 		colorImg;
+		ofxCvGrayscaleImage		grayImg;
+		ofxCvContourFinder		contourFinder;
+
+		vector< armBlob > arms;
+		float tolerance;
+		
+};
